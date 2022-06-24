e21c72797cecc26250e74ac8398017c7d3998e5c
waffle52 <asuka9767@protonmail.com>
2022-06-23 21:31:26 -0700

    Initial Commit

---
+++ Pc_Controller/Pc_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pc_ControllerApp.swift
+//  Pc_Controller
+//
+//  Created by Kyle Campbell on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Pc_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
